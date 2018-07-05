0ee1027d91734fffed24d8f706f59d1d7ec4f8ba
Chaitanya <chaits98@gmail.com>
2018-07-05 10:19:45 +0530

    Initial Commit

---
+++ android/app/src/main/java/com/fetchmusicfiles/react_native_fetch_music_files/react_native_fetch_music_filesModule.java
@@ -0,0 +1,57 @@
+//  Created by react-native-create-bridge
+
+package com.fetchmusicfiles.react_native_fetch_music_files;
+
+import android.support.annotation.Nullable;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.fetchmusicfiles.DataModels.SongCollection;
+import com.fetchmusicfiles.Fetch.FetchSongList;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class react_native_fetch_music_filesModule extends ReactContextBaseJavaModule {
+    public static final String REACT_CLASS = "react_native_fetch_music_files";
+    private static ReactApplicationContext reactContext = null;
+
+    public react_native_fetch_music_filesModule(ReactApplicationContext context) {
+        super(context);
+        reactContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put("EXAMPLE_CONSTANT", "example");
+
+        return constants;
+    }
+
+    @ReactMethod
+    public void fetchAllSongs(Callback errorCallback,
+                              Callback successCallback) {
+        FetchSongList.getInstance().getAllSongs(reactAppContext);
+
+        if (SongCollection.getInstance().getListOfSongs().isEmpty()) {
+            errorCallback.invoke("There are no Audio Files Found on This Device");
+            return;
+        }
+
+        successCallback.invoke(new Gson().toJson(SongCollection.getInstance().getListOfSongs()));
+    }
+
+    private static void emitDeviceEvent(String eventName, @Nullable WritableMap eventData) {
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, eventData);
+    }
+}
---
+++ react_native_fetch_music_filesNativeModule.js
@@ -0,0 +1,13 @@
+//  Created by react-native-create-bridge
+
+import { NativeModules } from 'react-native'
+
+const { react_native_fetch_music_files } = NativeModules
+
+export default {
+  fetchAllSongs () {
+    return react_native_fetch_music_files.fetchAllSongs()
+  },
+
+  EXAMPLE_CONSTANT: react_native_fetch_music_files.EXAMPLE_CONSTANT
+}
