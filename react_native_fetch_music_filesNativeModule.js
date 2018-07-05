ace45dbf10fd017de73023a7b273986987513f46
Chaitanya <chaits98@gmail.com>
2018-07-05 11:47:39 +0530

    fixed error Callback not defined in react_native_fetch_music_filesModule

--- react_native_fetch_music_filesNativeModule.js
+++ react_native_fetch_music_filesNativeModule.js
@@ -4,10 +4,4 @@ import { NativeModules } from 'react-native'
 
 const { react_native_fetch_music_files } = NativeModules
 
-export default {
-  fetchAllSongs () {
-    return react_native_fetch_music_files.fetchAllSongs()
-  },
-
-  EXAMPLE_CONSTANT: react_native_fetch_music_files.EXAMPLE_CONSTANT
-}
+export default react_native_fetch_music_files;
